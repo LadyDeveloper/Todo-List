8cf2d596723ecd428aeb649a653a7c07381b6bad
LadyDeveloper <anhacorrea@hotmail.com>
2020-07-07 09:24:33 -0700

    Final Project - missing database

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date =  require(__dirname + "/date.js");
 
 const app = express();
 
+const items = ["Create Exercise routine", "Create Coding routine", "List of meal prep"];
+
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send('Welcome to the TODO List');
+    const day = date.getDate();
+    res.render('list', {kindOfDay: day, itemsList: items});
+});
+
+app.post("/", (req, res) => {
+    const item = req.body.itemTodo;
+    items.push(item);
+    res.redirect("/");
 })
 
 app.listen(3000, () => {
